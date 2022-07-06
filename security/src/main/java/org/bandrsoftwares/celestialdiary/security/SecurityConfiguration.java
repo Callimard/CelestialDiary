a10a119bd955a6e29fcf0fb86a2216d8c2561165
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-07-06 19:43:04 +0200

    Update SecurityConfiguration

--- security/src/main/java/org/bandrsoftwares/celestialdiary/security/SecurityConfiguration.java
+++ security/src/main/java/org/bandrsoftwares/celestialdiary/security/SecurityConfiguration.java
@@ -1,17 +1,12 @@
 package org.bandrsoftwares.celestialdiary.security;
 
-import com.google.common.collect.Lists;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -25,16 +20,4 @@ import java.util.List;
 public class SecurityConfiguration {
 
     private List<String> corsOrigins;
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Lists.newArrayList(corsOrigins));
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return source;
-    }
 }
