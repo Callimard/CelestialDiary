46b4a6b77903e59232b46c30f2525332343fe5de
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-07-08 11:58:09 +0200

    Create the main Application page
    After that the employee has been connected, he is
    redirected to the application page which allow him
    to use Celestial Diary.
    
    For the moment, only the side bar has been
    implemented and allow the logout and show
    some account information.
    
    The side bar can be wrapped and in this case
    only displays icons.

---
+++ ui-frontend/src/service/authentication/jwt-account.ts
@@ -0,0 +1,11 @@
+export interface JwtAccount {
+
+  companyId: string;
+  companyEmail: string;
+  companyName: string;
+  employeeEmail: string;
+  employeeFirstName: string;
+  employeeLastName: string;
+  accountAuthorities: string[];
+
+}
