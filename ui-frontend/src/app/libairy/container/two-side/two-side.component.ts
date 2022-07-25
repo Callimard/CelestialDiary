8764810c5fb7ab5172f7824fbe234b13ab844331
Guillaume Rakotomalala <guillaume.rakotomalala@sealandblock.fr>
2022-07-25 10:14:47 +0200

    Refract UI component library

--- ui-frontend/src/app/libairy/container/two-side/two-side.component.ts
+++ ui-frontend/src/app/libairy/container/two-side/two-side.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ScreenService} from "../../../service/screen/screen.service";
+import {ScreenService} from "../../../../service/screen/screen.service";
 
 @Component({
   selector: 'app-two-side',
